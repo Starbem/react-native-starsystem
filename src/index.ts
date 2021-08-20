3d108bef35da70f92fb8bd82c29c8292225ffeb0
Julio Augusto <julioams@icloud.com>
2021-08-20 15:50:09 -0300

    feat: clear export components

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
-// UI references
-// https://ionicframework.com/docs/components/#buttons
-// https://material.io/guidelines/components/buttons.html#buttons-raised-buttons
-// https://material.angularjs.org/latest/demo/button
-// Core
-// import AirbnbRating, {TapRatingProps} from './AirbnbRating';
 import Avatar, {AvatarProps} from './Avatar';
 import Badge, {BadgeProps, withBadge} from './Badge';
 import BottomSheet, {BottomSheetProps} from './BottomSheet';
 import Button, {ButtonProps} from './Button';
-// import ButtonGroup, {ButtonGroupProps} from './ButtonGroup';
 import Card, {CardProps} from './Card';
 import CheckBox, {CheckBoxProps} from './CheckBox';
-// import Chip, {ChipProps} from './Chip';
-// import Dialog, {DialogLoadingProps, DialogTitleProps} from './Dialog';
 import Divider, {DividerProps} from './Divider';
-// import FAB, {FABProps} from './FAB';
 import Header, {HeaderProps} from './Header';
 import Icon, {IconProps} from './Icon';
 import Image, {ImageProps} from './Image';
@@ -25,24 +15,10 @@ import ListItem, {
   ListItemAccordionProps,
   ListItemSwipeableProps,
 } from './ListItem';
-// import Overlay, {OverlayProps} from './Overlay';
-// import PricingCard, {PricingCardProps} from './PricingCard';
-// import Rating, {SwipeRatingProps} from './Rating';
-// import SearchBar, {
-//   SearchBarProps,
-//   SearchBarAndroidProps,
-//   SearchBarDefaultProps,
-//   SearchBarIosProps,
-// } from './SearchBar';
-// import Slider, {SliderProps} from './Slider';
-// import SocialIcon, {SocialIconProps} from './SocialIcon';
-// import SpeedDial, {SpeedDialActionProps, SpeedDialProps} from './SpeedDial';
 import Switch, {SwitchProps} from './Switch';
 import Tab, {TabItemProps, TabProps} from './Tab';
 import TabView, {TabViewProps} from './TabView';
 import Text, {TextProps} from './Text';
-// import Tile, {TileProps} from './Tile';
-// import Tooltip, {TooltipProps} from './Tooltip';
 
 import {
   Colors,
@@ -70,37 +46,23 @@ import {
 
 // Components exports
 export {
-  // AirbnbRating,
   Avatar,
   Badge,
   BottomSheet,
   Button,
-  // ButtonGroup,
   Card,
   CheckBox,
-  // Chip,
-  // Dialog,
   Divider,
-  // FAB,
   Header,
   Icon,
   Image,
   Input,
   LinearProgress,
   ListItem,
-  // Overlay,
-  // PricingCard,
-  // Rating,
-  // SearchBar,
-  // Slider,
-  // SocialIcon,
-  // SpeedDial,
   Switch,
   Tab,
   TabView,
   Text,
-  // Tile,
-  // Tooltip,
 };
 
 // Theme utils exports
@@ -126,16 +88,11 @@ export type {
   AvatarProps,
   BadgeProps,
   BottomSheetProps,
-  // ButtonGroupProps,
   ButtonProps,
   CardProps,
   CheckBoxProps,
-  // ChipProps,
   Colors,
-  // DialogLoadingProps,
-  // DialogTitleProps,
   DividerProps,
-  // FABProps,
   HeaderProps,
   IconProps,
   ImageProps,
@@ -144,25 +101,11 @@ export type {
   ListItemAccordionProps,
   ListItemProps,
   ListItemSwipeableProps,
-  // OverlayProps,
-  // PricingCardProps,
-  // SearchBarAndroidProps,
-  // SearchBarDefaultProps,
-  // SearchBarIosProps,
-  // SearchBarProps,
-  // SliderProps,
-  // SocialIconProps,
-  // SpeedDialActionProps,
-  // SpeedDialProps,
-  // SwipeRatingProps,
   SwitchProps,
   TabItemProps,
   TabProps,
   TabViewProps,
-  // TapRatingProps,
   TextProps,
-  // TileProps,
-  // TooltipProps,
 };
 
 // Theme props export
